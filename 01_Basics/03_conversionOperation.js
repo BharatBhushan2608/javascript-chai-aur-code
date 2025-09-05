9c37bfedd45946af9a01dee31cfd37a85cbfa242
BharatBhushan2608 <e22cseu1666@bennett.edu.in>
2025-09-06 02:07:07 +0530

    conversion of datatypes and operations

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,103 @@
+        //CONVERSION OF DATATYPES
+
+// let score = "33abc"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof (valueInNumber))
+// console.log((valueInNumber))
+
+
+// let score1 = null
+// let valueInNumber1 = Number(score1)
+// console.log(score1)
+// console.log(valueInNumber1)
+
+
+// let score2 = undefined
+// let valueInNumber2 = Number(score2)
+// console.log(score2);
+// console.log(valueInNumber2)
+
+// let score3 = true
+// let valueInNumber3 = Number(score3)
+// console.log(score3);
+// console.log(valueInNumber3)
+
+//   " 33" = 33
+//  "33abc" = NaN
+//    ture  = 1 ; false = 0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof (isLoggedIn));
+// console.log((booleanIsLoggedIn));
+
+let isLoggedIn1 = ""
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
+// console.log((booleanIsLoggedIn1));
+
+let isLoggedIn2 = "Bharat"
+let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
+// console.log((booleanIsLoggedIn2));
+
+
+
+// 1 = true ; 0 = false
+// " " = false
+// "bharat" = true
+
+let someNumber = 33
+
+let stringNumber  = String (someNumber)
+
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+// ***************OPERATIONS*************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2) ;
+// console.log(2-2) ;
+// console.log(2*2) ;
+// console.log(2**2) ;
+// console.log(2/3) ;
+// console.log(2%3) ;
+
+let str1 = "bharat"
+let str2 = "bhushan"
+// console.log(str1 + str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log( 2 + 2 + "1");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+//++gameCounter;
+console.log(gameCounter);
+
+let gameCounter1 = 100
+++gameCounter1;
+console.log(gameCounter1);
+
+
+
+
