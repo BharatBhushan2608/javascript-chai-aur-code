bb6eff9127e3a817a57d768b9273e7cea443a5fa
BharatBhushan2608 <e22cseu1666@bennett.edu.in>
2025-09-06 02:37:05 +0530

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,35 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+
+//keep n mind that use always same types of datatypes to compare between them
+// console.log("2" > 1);
+// it convert atomatically this number("2") into 2 then it compare
+// console.log("02" > 1);
+
+/* avoid this types of coding
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0);  //true
+comaprision convert null to a number , treating it as 0
+ 
+this (==) is work different than this(>=,<=) in java script
+
+
+console.log(undefined > 0); //false
+console.log(undefined == 0); //false
+console.log(undefined < 0); //false 
+
+*/
+
+// strictly check (===)
+console.log("2" === 2); //because datatypes of both is not same
+
+
+
+
