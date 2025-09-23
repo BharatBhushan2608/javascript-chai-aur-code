c7433589d140f0d95c79553fe0702943ec84ae8c
BharatBhushan2608 <e22cseu1666@bennett.edu.in>
2025-09-23 19:28:15 +0530

    DIGITAL CALCULATOR solution

---
+++ ProjectsWithJs/03_DigitalClock/script.js
@@ -0,0 +1,12 @@
+document.getElementById('clock')
+const clock = document.getElementById('clock')
+console.log(clock)
+
+let date = new Date();
+console.log(date.toLocaleTimeString());
+
+setInterval(function (){
+    let date = new Date();
+    //console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+},1000)
