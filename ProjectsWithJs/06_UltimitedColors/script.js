d114af4a15d349b69fbb56805e8268a1c397ec07
BharatBhushan2608 <e22cseu1666@bennett.edu.in>
2025-09-26 17:48:13 +0530

    unlimited color changing

---
+++ ProjectsWithJs/06_UltimitedColors/script.js
@@ -0,0 +1,40 @@
+// Generate a random color 
+
+const randomColor = function  ( ) {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
+        color += hex[(Math.floor(Math.random() * 16))]
+        
+    }
+    return color
+};
+
+// console.log (randomColor()); // if u wnat see random color that u generate 
+
+let intervalid;
+const startChangingColor = function  ( ) {
+
+
+    if (!intervalid) {
+        intervalid = setInterval(chnageBgColor , 1000)
+    }
+     
+    function  chnageBgColor( ) {
+        document.body.style.backgroundColor = randomColor();
+        
+    } 
+}
+
+
+const stopChangingColor = function  ( ) {
+    clearInterval(intervalid);
+    intervalid = null;
+}
+
+document.querySelector('#start').addEventListener('click' , startChangingColor)
+
+
+document.querySelector('#stop').addEventListener('click' , stopChangingColor)
+
