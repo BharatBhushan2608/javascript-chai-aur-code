67326c9a6717370eec17ad5f92b5c09b605a60c8
BharatBhushan2608 <e22cseu1666@bennett.edu.in>
2026-01-19 15:34:45 +0530

    revision of arrow function

--- 03_Basics/03_Arrow.js
+++ 03_Basics/03_Arrow.js
@@ -3,8 +3,8 @@ const user = {
     price : 9999,
 
     welcomeMessage: function(){ // upr wala value to access krne k liye this use krte h
-        // console.log(`${this.username} , welcome to website `); // this current context (mtlb varaible) to refer krta hai 
-        // console.log(this);
+        console.log(`${this.username} , welcome to website `); // this current context (mtlb varaible) to refer krta hai 
+        console.log(this);
         
     }
 
@@ -14,7 +14,7 @@ const user = {
 // user.username = "bhushan"
 // user.welcomeMessage()
 
-//console.log(this) // iska output => {}
+// console.log(this) // iska output => {}
 
 function cahi(){
 
@@ -23,26 +23,26 @@ function cahi(){
 }
 // cahi()
 
-// function cahi_1(){
-//     let username = "bharat"
-//     // console.log(this.username); // fonction k andr this kuch v print nhi krega
+function cahi_1(){
+    let username = "bharat"
+    console.log(this.username); // function k andr this kuch v print nhi krega
     
-// }
-// // cahi_1()
+}
+// cahi_1()
 
 
-// const chai = function(){
-//     let username = "bharat"
-//     console.log(this.username);  
-// }
-// chai()
+const chai_2 = function(){
+    let username = "bharat"
+    console.log(this.username);  
+}
+// chai_2()
 
 //ARROW FUNCTION DECLEARATION
 
-// const chai = () => {
-//     let username = "bharat"
-//     console.log(this);  
-// }
+const chai = () => {
+    let username = "bharat"
+    console.log(this);  
+}
 // chai()
 
 // const addTwo = (num1 , num2) => {
@@ -60,7 +60,7 @@ function cahi(){
 // object kaise return hoga
 const addTwo = (num1 , num2) =>  ({username : "bharat" }) 
 
-console.log(addTwo( 3 , 4));
+//console.log(addTwo( 3 , 4));
 
 
 // const myArray = [2,3,4,5,6]
